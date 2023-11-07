746234e0243b73f3c286ab1b76ff4c59fd388266
frendguo <frendguo@live.cn>
2023-11-07 15:28:15 +0800

    fix link in config

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'NTDev',
   author: 'Frend Guo',
   email: 'i@ntdev.cn',
-  link: 'https://blog.ntdev.cn',
+  link: 'https://blogs.ntdev.cn',
   description: 'Developing Nt...',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
