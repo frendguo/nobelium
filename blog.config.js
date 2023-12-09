9bcb8742ab83ae3a964c778c291a539014d8db36
frendguo <frendguo@live.cn>
2023-12-09 18:38:34 +0800

    Update blog.config.js
    修改成 frendguo.com

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'NTDev',
+  title: 'Frendguo blog',
   author: 'Frend Guo',
-  email: 'i@ntdev.cn',
-  link: 'https://blogs.ntdev.cn',
+  email: 'i@guoshi.xin',
+  link: 'https://frendguo.com',
   description: 'Developing Nt...',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: ['en-US', 'zh-CN'], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
