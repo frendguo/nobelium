da24a2dfb833eaae531d69cf43aa8b342fe1c3f1
frendguo <frendguo@live.cn>
2024-08-13 19:42:21 +0800

    Add console log statement in Container.js

--- components/Container.js
+++ components/Container.js
@@ -78,6 +78,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
       </Head>
       <Script onReady={
         () => {
+          console.log('hahaha');
           (function (c, l, a, r, i, t, y) {
             c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
             t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
