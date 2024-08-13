1e4f2629ef32e97895b1bc6aedab7f675eabccf3
frendguo <frendguo@live.cn>
2024-08-13 19:24:28 +0800

    update Clarity

--- components/Container.js
+++ components/Container.js
@@ -15,19 +15,22 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
     type: 'website',
     ...customMeta
   }
+
+  useEffect(() => {
+    console.log('Clarity Analytics is enabled')
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "nmet4f36z7")
+  }, [])
+
   return (
     <div>
       <Head>
         <title>{meta.title}</title>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9528485090996946" crossorigin="anonymous"></script>
         {/* <meta content={BLOG.darkBackground} name="theme-color" /> */}
-        <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "nmet4f36z7");
-        </script>
         <meta name="robots" content="follow, index" />
         <meta charSet="UTF-8" />
         {BLOG.seo.googleSiteVerification && (
