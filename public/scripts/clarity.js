2951431c5054198d19a9752afe1b9bca14faf94a
frendguo <frendguo@live.cn>
2024-08-17 11:21:06 +0800

    add indexnow

--- public/scripts/clarity.js
+++
@@ -1,6 +0,0 @@
-(function (c, l, a, r, i, t, y) {
-    console.log('clarity.js');
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i; t.crossorigin = "anonymous";
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-})(window, document, "clarity", "script", "nmet4f36z7")
